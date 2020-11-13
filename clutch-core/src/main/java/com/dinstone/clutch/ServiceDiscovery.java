deca3cd6cbddac4f505574404079cfafcd4450e8
dinstone <dinstone@localhost>
2020-11-13 18:06:23 +0800

    format code

--- clutch-core/src/main/java/com/dinstone/clutch/ServiceDiscovery.java
+++ clutch-core/src/main/java/com/dinstone/clutch/ServiceDiscovery.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014~2017 dinstone<dinstone@163.com>
+ * Copyright (C) 2014~2020 dinstone<dinstone@163.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.util.List;
  * Service Discovery
  * 
  * @author dinstone
+ * 
  * @version 1.0.0
  */
 public interface ServiceDiscovery {
--- clutch-core/src/main/java/com/dinstone/clutch/ServiceRegistry.java
+++ clutch-core/src/main/java/com/dinstone/clutch/ServiceRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014~2017 dinstone<dinstone@163.com>
+ * Copyright (C) 2014~2020 dinstone<dinstone@163.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package com.dinstone.clutch;
  * Service Registry
  * 
  * @author dinstone
+ * 
  * @version 1.0.0
  */
 public interface ServiceRegistry {
@@ -27,9 +28,10 @@ public interface ServiceRegistry {
      * Register / Re-register a service instance
      * 
      * @param service
-     *        the service instance description
+     *            the service instance description
+     * 
      * @throws Exception
-     *         errors
+     *             errors
      */
     public void register(ServiceDescription service) throws Exception;
 
@@ -37,9 +39,10 @@ public interface ServiceRegistry {
      * Deregister / Remove a service instance
      * 
      * @param service
-     *        the service instance description
+     *            the service instance description
+     * 
      * @throws Exception
-     *         errors
+     *             errors
      */
     public void deregister(ServiceDescription service) throws Exception;
 
